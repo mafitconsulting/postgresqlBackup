e2d8336418ab734600ddec696769c61faf924110
Mark Fieldhouse <Mark.Fieldhouse@mafitconsulting.co.uk>
2018-01-27 11:31:12 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version='0.1.0',
     description='Database backups locally or to AWS S3.',
     long_description=readme,
-    author='Keith Thompson',
-    author_email='keith@linuxacademy.com',
+    author='Mark Fieldhouse'
+    author_email='Mark.Fieldhouse@mafitconsulting.co.uk'
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['boto3'],
